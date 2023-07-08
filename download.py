76ba0161ba1fccfcc4c7120ec58d64e2ef137be2
naielv <109038805+naielv@users.noreply.github.com>
2023-07-08 16:59:02 +0200

    Actualizar download.py

--- download.py
+++ download.py
@@ -1,13 +1,13 @@
 import os
 
-def dfile: str, args: str = ""):
+def dfile: str, args: str = "", out: str = "./videos/"):
   with open(file, "r") as f:
     for line in f.read().splitlines():
       try:
         os.system(f"yt-dlp {args} {line}")
       except Exception as e:
         print(e)
-def dyear(year: int, args: str = ""):
+def dyear(year: int, args: str = "", out: str = "./videos/"):
   y = str(year)
   file = f"./playlist/{year}.txt"
   dfile(file, args)
